88a08a585207025824b04002a62cc0217b059caf
Arpan Kumar <arpan.main@outlook.com>
2026-03-11 23:03:20 +0530

    add Auth Middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(400).json({
+        message: "Unauthorized, no token found",
+        success: false,
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(404).json({
+        message: "Unauthorized",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ _id: decoded._id }).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        message: "No user found",
+        success: false,
+      });
+    }
+
+    req.user = user;
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: error,
+      success: false,
+    });
+  }
+};
+export default authMiddleware;
