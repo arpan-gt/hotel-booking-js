f7bc6e90d1b6d544f7e8b2f34bfb03ed71419a14
Arpan Kumar <arpan.main@outlook.com>
2026-03-11 17:40:57 +0530

    add user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,63 +1,83 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Invalid email address");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a stronger password");
+        }
+      },
+    },
+
+    dateOfBirth: {
+      type: Date,
+      required: true,
+      validate(value) {
+        const today = new Date();
+
+        if (value > today) {
+          throw new Error("Date of birth cannot be in future");
+        }
+
+        let age = today.getFullYear() - value.getFullYear();
+        const monthDiff = today.getMonth() - value.getMonth();
+
+        if (
+          monthDiff < 0 ||
+          (monthDiff === 0 && today.getDate() < value.getDate())
+        ) {
+          age--;
+        }
+
+        if (age < 18) {
+          throw new Error("User must be at least 18 years old");
+        }
+      },
+    },
+
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+      required: true,
     },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (!validator.isStrongPassword(value)) {
-        throw new Error("Enter a stronger password");
-      }
-    },
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  dateOfBirth: {
-    type: Date,
-    required: true,
-    validate(value) {
-      const today = new Date();
-      if (value > today) {
-        throw new Error("Date of birth cannot be in the future");
-      }
+);
 
-      let age = today.getFullYear() - value.getFullYear();
-      const monthDiff = today.getMonth() - value.getMonth();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-      if (
-        monthDiff < 0 ||
-        (monthDiff === 0 && today.getDate() < value.getDate())
-      ) {
-        age--;
-      }
-
-      if (age < 18) {
-        throw new Error("You must be at least 18 years old");
-      }
-    },
-  },
-  gender: {
-    type: String,
-    enum: ["Male", "Female"],
-    required: true,
-  },
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-const User = mongoose.model("User", userSchema)l
+const User = mongoose.model("User", userSchema);
+
 export default User;
